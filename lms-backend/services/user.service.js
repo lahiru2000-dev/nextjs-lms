69c50e599c3c93432120bd356d60191ec4698c83
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-06 11:30:31 +0530

    added more fields to register

--- lms-backend/services/user.service.js
+++ lms-backend/services/user.service.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 //register user
 const registerUser = async (req, res) => {
     try{
-        const {name, email, password} = req.body;
+        const {name, email, password, role, grade} = req.body;
         //validation
         if(!name || !email || !password){
             return res.status(400).json({
@@ -16,7 +16,9 @@ const registerUser = async (req, res) => {
         }
 
         //check email already exists
-        const existingUser = await pool.query(`SELECT * FROM users WHERE email=$1`,[email]);
+        const existingUser = await pool.query(
+            `SELECT * FROM users WHERE email=$1`,
+            [email]);
         if(existingUser.rows.length > 0){
             return res.status(400).json({
                 success:false,
@@ -25,15 +27,24 @@ const registerUser = async (req, res) => {
         }
         //hashed password
         const hashedPassword = await bcrypt.hash(password, 10);
-        const result= await pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,[name, email, hashedPassword]);
+         const result = await pool.query(
+            `INSERT INTO users (name, email, password, role, grade)
+             VALUES ($1, $2, $3, $4, $5)
+             RETURNING id, name, email, role, grade`,
+            [
+                name,
+                email,
+                hashedPassword,
+                role || "student",
+                grade || null
+            ]
+        );
+        const user = result.rows[0];
         return res.status(201).json({
             success:true,
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email
-            }
+            user
         });
+
     
     }catch(err){
         console.error(err);
