d9424553c388c2a7bb5df801e2d3e7507dc210bd
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-10 11:42:52 +0530

    register function and default form

---
+++ lms-frontend/app/auth/register/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useState } from "react";
+import React from "react";
+import axios from "axios";
+
+
+type registerType = {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  grade: string;
+}
+function register() {
+    //usestate
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [role, setRole] = useState<string>("student");
+    const [grade, setGrade] = useState<string>("");
+
+    //register function
+    const register=async()=>{
+      try {
+        const payload={name,email,password,role,grade};
+        const res=await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`,payload);
+        alert("Registration successful");
+        console.log(res.data);
+        
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+
+
+
+  return (
+    <div>
+      <h1>register</h1>
+      <form>  
+        <input type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="text" placeholder="role" value={role} onChange={(e) => setRole(e.target.value)} />
+        <input type="text" placeholder="grade" value={grade} onChange={(e) => setGrade(e.target.value)} />
+        <button type="submit">register</button>
+      </form>
+    </div>
+  )
+}
+
+export default register
+
+
+
+
+
