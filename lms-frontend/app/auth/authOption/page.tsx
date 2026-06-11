7fd255791ece1e50123fcee14f881b6e365aa45c
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-11 11:35:53 +0530

    intial authoption page

---
+++ lms-frontend/app/auth/authOption/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+import {useRouter} from "next/navigation";
+
+function AuthOption() {
+    const router = useRouter();
+
+
+
+  return (
+    <div className="min-h-screen flex font-sans overflow-hidden">
+        {/* ── Left Panel ── */}
+
+
+    </div>
+  )
+}
+
+export default AuthOption
