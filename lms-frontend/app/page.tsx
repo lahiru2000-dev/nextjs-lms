b559c22479f12e65eb3042137a16f82a66706a55
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-15 12:21:34 +0530

    add teacher component to home page

--- lms-frontend/app/page.tsx
+++ lms-frontend/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Herosection from "@/components/Herosection";
+import Featured from "@/components/Featured";
+import Teachers from "@/components/Teachers";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans ">
       <Herosection />
+      <Featured />
+      <Teachers />
     
     </div>
   );
