11e45a470fcbfb3ead28caeba750cbab735f60fb
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-16 11:14:04 +0530

    navbar make transparent

--- lms-frontend/components/Navbar.tsx
+++ lms-frontend/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="absolute top-0 left-0 right-0 z-50 bg-transparent">
       <div className="max-w-7xl mx-auto px-6">
         <div className="flex justify-between items-center h-16">
           
