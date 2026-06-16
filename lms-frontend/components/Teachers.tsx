3d91566ad6254fc4ba0b48b0f6ecc7cae769c8c5
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-16 11:56:23 +0530

    refctor

--- lms-frontend/components/Teachers.tsx
+++ lms-frontend/components/Teachers.tsx
@@ -33,7 +33,7 @@ function Teachers() {
    <div className="w-full px-6 py-6 bg-gray-50">
     <div className="max-w-6xl mx-auto">
       <div className="mb-6">
-        <h1 className="text-4xl font-bold text-gray-900 tracking-tight">Subjects</h1>
+        <h1 className="text-4xl font-bold text-gray-900 tracking-tight">Tutors</h1>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
         {teachers.map((teacher) => (
