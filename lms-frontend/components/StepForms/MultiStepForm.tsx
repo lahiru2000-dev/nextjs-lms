ca524d23cc6f6f774dc3888511bf7ee18d817962
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-17 16:21:24 +0530

    add interface to main form component

---
+++ lms-frontend/components/StepForms/MultiStepForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React, { useState } from "react";
+import Step1 from "./Step1";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
+
+interface AddCourseFormProps{
+    teacherId:string;
+}
