7e1ed0a8946749f800743bbf9678837c6e985b99
lahiru2000-dev <phlahirusampath2000@gmail.com>
2026-06-22 11:31:18 +0530

    course type checking using interface

--- lms-frontend/app/teacherDashboard/page.tsx
+++ lms-frontend/app/teacherDashboard/page.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useRouter} from "next/navigation";
+
+interface Course{
+  id: number;
+  course_name: string;
+  course_code: string;
+  description: string;
+  grade: string;
+  thumbnail_url: string | null;
+  video_url: string | null;
+  created_at: string;
+}
 
 function TeacherDashboard() {
+
+
+
+
+
+
+
+  
   return (
     <div>page</div>
   )
